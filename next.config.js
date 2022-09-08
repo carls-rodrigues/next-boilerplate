7d2d1f8155dc0e46da320c9ae803e933ca6ac748
Carlos Rodrigues <cerf@furg.br>
2022-09-08 19:52:14 -0300

    update dependencies and fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache.js')
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   swcMinify: false,
+//   experimental: {
+//     // Enables the styled-components SWC transform
+//     styledComponents: true
+//   },
+//   pwa: {
+//     dest: 'public',
+//     disable: !isProd
+//   }
+// })
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
   experimental: {
     // Enables the styled-components SWC transform
@@ -10,6 +27,20 @@ module.exports = withPWA({
   },
   pwa: {
     dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
     disable: !isProd
   }
-})
+}
+
+const buildConfig = () => {
+  const plugins = [withPWA]
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig
+  })
+  return config
+}
+
+module.exports = buildConfig
